1977bca3e38c00706280a2aeb53be136b2cd7856
Joe Harrow <jwharrow@gmail.com>
2017-02-24 11:39:23 -0600

    Add ship model

--- Models/ship.js
+++ Models/ship.js
@@ -1,1 +1,17 @@
 //ship class with all of the ships associated functions
+var shipDefaults = {
+  x: 100,
+  y: 100,
+  height: 10,
+  width: 10,
+  rad: -(Math.PI/2),
+}
+
+function Ship() {
+  this.x = shipDefaults.x;
+  this.y = shipDefaults.y;
+  this.height = shipDefaults.height;
+  this.width = shipDefaults.width;
+  this.rad = shipDefaults.rad;
+  this.class = "ship";
+}

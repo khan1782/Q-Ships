c6e79be41a021d81c5a6021415d0d80c23c5dc8d
Joe Harrow <jwharrow@gmail.com>
2017-02-24 15:47:11 -0600

    Add ship attributes for flight. Add movement method without limit

--- Models/ship.js
+++ Models/ship.js
@@ -5,7 +5,11 @@ var shipDefaults = {
   height: 10,
   width: 10,
   rad: -(Math.PI/2),
-}
+  initialDx: 0, 
+  initialDy: 0,
+  thrust: .25,
+  rotate: (Math.PI/6),
+};
 
 function Ship() {
   this.x = shipDefaults.x;
@@ -14,4 +18,30 @@ function Ship() {
   this.width = shipDefaults.width;
   this.rad = shipDefaults.rad;
   this.class = "ship";
-}
+  this.keys = {
+    up: false,
+    left: false,
+    right: false
+  };
+  this.dx = shipDefaults.initialDx;
+  this.dy = shipDefaults.initialDy;
+  this.thrust = shipDefaults.thrust;
+  this.rotate = shipDefaults.rotate;
+};
+
+Ship.prototype.navigateTheStars = function() {
+  if (this.keys.up === true) {
+    this.dx += this.thrust * Math.cos(this.rad);
+    this.dy += this.thrust * Math.sin(this.rad);  
+  }
+  if (this.keys.left === true) {
+    this.angle -= this.rotate;
+  }
+  if (this.keys.right === true) {
+    this.angle += this.rotate; 
+  }
+};
+
+Ship.prototype.speed = function() {
+  
+};

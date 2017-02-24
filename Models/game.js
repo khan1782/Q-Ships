d1511508f4072f4193e71c4518a40c0bf23e26d7
Joe Harrow <jwharrow@gmail.com>
2017-02-24 11:18:08 -0600

    Add game class and snapshot function

--- Models/game.js
+++ Models/game.js
@@ -1,1 +1,16 @@
 //Game Class and associated functions
+var STATES = {
+  run: 0
+}
+
+function Game() {
+ this.width = 1000;
+ this.height = 1000;
+ this.state = STATES.run;
+ this.items = [];
+}
+
+Game.prototype.snapshot = function() {
+  return JSON.stringify(this);
+}
+

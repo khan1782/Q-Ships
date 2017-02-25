13de252030458d07d8707c8036b90e7c57cbf477
kevin <khan1782@gmail.com>
2017-02-25 17:00:55 -0600

    ASD

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -2,108 +2,107 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   //identify starfield div container
   var background = document.getElementById('background');
+
   //initialize new starfield beep beep boop
   var starfield = new Starfield();
+
   //run initialize on initialized starfield
   starfield.initialize(background);
+
   //begin starfield loop
   starfield.start();
 
 
-//-----------------Server Side----------------------
-  var game = new Game()
-  game.players.push(new Player)
-  game.players[0].ship = new Ship
-  game.gameLoop()
-//--------------------------------------------------
-
+//------------will receive from server------------
   var currentSnapshot = function() {
     return JSON.parse(game.snapshot())
   }
+//------------------------------------------------
 
   //identify game canvas
   var canvas = document.getElementById("gameCanvas")
+
   //initialize renderer machinery *wa wa wa wa*
   render = new Renderer(canvas, currentSnapshot())
+
   //set game bounds
   render.initialize()
-  //start of the game loop (running draw at 50fps)
-  //update the assets per loop
 
-
-
-  //snapshot is captured
-
-
-  // render.gameLoop(currentSnapshot());
+  //run game loop
   render.gameLoop(currentSnapshot);
+
   //add listeners for key strokes for initialized game
   keyStrokeListeners(game)
  
-
-
-
 })
 
 
-// game.updateEntity({id: "jf9324j32", keys: [true, true, false, false]})
-// ws.sendMessage(JSON.stringify({messageType: "entityUpdate", update: {id: "jf9324j32", keys: [true, true, false, false]}})
-//sets up event listeners for up, down, left, right
 
 function keyStrokeListeners(game) {
+  //json package ready for editing
+  var keys = {keys: {up: false, down: false, left: false, right: false}}
+  
   //document event listener for keydown
-  var keys = {up: false, down: false, left: false, right: false}
-
-  document.addEventListener('keydown', function(event){
+    document.addEventListener('keydown', function(event){
     //listening for up
-    if(event.keyCode === 38 && keys.up === false) {
-      keys.up = true;
+    if(event.keyCode === 38 && keys.keys.up === false) {
+      keys.keys.up = true;
       // send json to server controller
-      //SEND THIS!!! JSON.stringify(keys)
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
+
     }
     //listening for down
-    if(event.keyCode === 40 && keys.down === false ) {
-      keys.down = true;
+    if(event.keyCode === 40 && keys.keys.down === false ) {
+      keys.keys.down = true;
       // send json to server controller
-       //SEND THIS!!! JSON.stringify(keys)
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
 
     //listening for left
-    if(event.keyCode === 37 && keys.left === false) {
-      keys.left = true;
+    if(event.keyCode === 37 && keys.keys.left === false) {
+      keys.keys.left = true;
       // send json to server controller
-       //SEND THIS!!! JSON.stringify(keys)
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
 
     // listening for right
-    if(event.keyCode === 39 && keys.right === false ) {
-      keys.right = true;
+    if(event.keyCode === 39 && keys.keys.right === false ) {
+      keys.keys.right = true;
       // send json to server controller
-      //SEND THIS!!! JSON.stringify(keys)
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
   });
 
   //document event listener for keydown to reset keys to false
   document.addEventListener('keyup', function(event){
     if(event.keyCode === 38) {
-      keys.up = false;
-       //SEND THIS!!! JSON.stringify(keys)
+      keys.keys.up = false;
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
     if(event.keyCode === 40) {
-      keys.down = false;
-       //SEND THIS!!! JSON.stringify(keys)
+      keys.keys.down = false;
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
     if(event.keyCode === 37) {
-      keys.left = false;
-       //SEND THIS!!! JSON.stringify(keys)
+      keys.keys.left = false;
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
     if(event.keyCode === 39) {
-      keys.right = false;
-       //SEND THIS!!! JSON.stringify(keys)
+      keys.keys.right = false;
+      websock.package = JSON.stringify(keys)
+      websock.sent = true
     }
     if(event.keyCode === 32) {
       // send a missle json that is stamped with a user id
       //SEND THIS!!!! "fire"
+      websock.package = JSON.stringify({fire:"pew"})
     }
   });
 }

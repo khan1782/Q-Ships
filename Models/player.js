cf2fa9d6340bfeb582294e41969f0d03b508d34c
Joe Harrow <jwharrow@gmail.com>
2017-02-25 18:02:59 -0600

    update uuid with pews

--- Models/player.js
+++ Models/player.js
@@ -16,6 +16,6 @@ function uuid() {
       .toString(16)
       .substring(1);
   }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+  return s4() + s4() + 'pew' + s4() + 'pew' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
 }
